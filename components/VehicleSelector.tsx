d47af6f9286c6416de5f3444da6d07c3a63f53c5
Ron June Valdoz <ronjune.lopez@gmail.com>
2023-09-15 22:11:50 +0800

    Fix callback interface

--- components/VehicleSelector.tsx
+++ components/VehicleSelector.tsx
@@ -1,24 +1,18 @@
-import { Row, Col, Button, ButtonGroup, Card } from "react-bootstrap";
+import React from "react";
+import { Button, ButtonGroup } from "react-bootstrap";
 
-const VehicleSelector = ({ onClick1, onClick2 }) => {
+interface VehicleSelectorProps {
+  onClick1: () => void;
+  onClick2: () => void;
+}
+
+const VehicleSelector: React.FC<VehicleSelectorProps> = ({ onClick1, onClick2 }) => {
   return (
     <>
       <h2>Select a Vehicle</h2>
       <ButtonGroup vertical>
-        <Button
-          onClick={() => {
-            onClick1();
-          }}
-        >
-          Tesla Model 3 Performance
-        </Button>
-        <Button
-          onClick={() => {
-            onClick2();
-          }}
-        >
-          BMW 130d xDrive 26d
-        </Button>
+        <Button onClick={onClick1}>Tesla Model 3 Performance</Button>
+        <Button onClick={onClick2}>BMW 130d xDrive 26d</Button>
       </ButtonGroup>
     </>
   );
